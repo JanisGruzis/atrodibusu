8302b64afdb469e3e1abd41947d411f36ab3ce79
Jānis Gruzis <jgruzis@gmail.com>
2015-03-25 22:57:29 +0200

    Removed demo stuff

--- src/AppBundle/Controller/StatisticsController.php
+++ src/AppBundle/Controller/StatisticsController.php
@@ -3,15 +3,17 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class StatisticsController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="statistics")
+	 * @Template()
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return [];
     }
 }
